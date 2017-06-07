fb3c67c07c34c046f994bdf73d197ba22421351a
cisco <cisco@cisco.com>
2017-06-07 13:45:07 -0700

    Added xml to add interface with secondary IP

--- course/module2/lab7/xr_nc_configure_ipinterface.py
+++ course/module2/lab7/xr_nc_configure_ipinterface.py
@@ -9,6 +9,14 @@ if __name__ == "__main__":
                          hostkey_verify=False, device_params={'name': 'iosxr'},
                          allow_agent=False, look_for_keys=False) as device:
 
+        nc_filter = """
+              <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg">
+               <interface-configuration>
+               </interface-configuration>
+            </interface-configurations>
+        """
+        nc_get_reply = device.get(('subtree', nc_filter))
+        print nc_get_reply
 
         nc_filter = """
             <config>
@@ -25,6 +33,25 @@ if __name__ == "__main__":
                   </primary>
                  </addresses>
                 </ipv4-network>
+               </interface-configuration>
+               <interface-configuration>
+                <active>act</active>
+                <interface-name>GigabitEthernet0/0/0/0</interface-name>
+                 <ipv4-network xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ipv4-io-cfg">
+                  <addresses>
+                   <primary>
+                    <address>10.23.23.1</address>
+                    <netmask>255.255.255.0</netmask>
+                   </primary>
+                   <secondaries>
+                    <secondary>
+                     <address>20.32.32.1</address>
+                     <netmask>255.255.255.0</netmask>
+                    </secondary>
+                   </secondaries>
+                  </addresses>
+                 </ipv4-network>
+                <shutdown></shutdown>
                </interface-configuration>
               </interface-configurations>
             </config>
