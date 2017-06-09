f62c8f69857cb307c03971a4e01bed97fedf2842
cisco <cisco@cisco.com>
2017-06-09 11:37:19 -0700

    Errors fixed, replaced with namespace removal function

--- course/module2/lab3/nxos_nc_show_version.py
+++ course/module2/lab3/nxos_nc_show_version.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 from ncclient import manager
+from lxml import etree
+from lxml.builder import ElementMaker
+
+def remove_namespace(xml):
+    for elem in xml.getiterator():
+        split_tag = elem.tag.split('}')
+        if len(split_tag) > 1:
+            elem.tag = split_tag[1]
+    return xml
 
 if __name__ == '__main__':
 
@@ -16,13 +25,17 @@ if __name__ == '__main__':
                       """
         nc_get_reply = device.get(('subtree', get_filter))
         print 'Response as XML String: '
-        print nc_get_reply.xmml
+        print nc_get_reply.xml
 
-        ns_map = {'mod': 'http://www.cisco.com/nxos:1.0:vdc_mgr'}
-        xml_rsp_cid = nc_get_reply.data_ele.find('.//mod:chassis_id', ns_map)
-        cid_value = xml_rsp_cid.text
-        xml_rsp_sw = nc_get_reply.data_ele.find('.//mod:sys_ver_str', ns_map)
+        nc_get_reply_no_ns = remove_namespace(nc_get_reply.data_ele)
+        xml_rsp_sw = nc_get_reply_no_ns.find('.//sys_ver_str')
+        xml_rsp_cid = nc_get_reply_no_ns.find('.//chassis_id')
+
+#        ns_map = {'mod': 'http://www.cisco.com/nxos:1.0:vdc_mgr'}
+#        xml_rsp_sw = nc_get_reply.data_ele.find('.//mod:sys_ver_str', ns_map)
         sw_value = xml_rsp_sw.text
+#        xml_rsp_cid = nc_get_reply.data_ele.find('.//mod:chassis_id', ns_map)
+        cid_value = xml_rsp_cid.text
         print '================================='
         print 'Chassis ID: ', cid_value
         print 'Software Version: ', sw_value
